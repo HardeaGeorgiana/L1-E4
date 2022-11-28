1472626136027b5f1413ed54c9d9ece6d7767396
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-28 11:09:35 +0200

    Add project files.

---
+++ L1 E4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace L1_E4
+{
+    /*Ex.4 Scrieti un program care va afisa semnul unui numar citit de la tastatura
+           • Daca numarul este pozitiv, va afisa “+”
+           • Daca numarul este negativ, va afisa “-”
+           • Daca numarul este 0, va afisa “0”
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numar;
+
+            Console.WriteLine("Introduceti un numar");
+            numar = int.Parse(Console.ReadLine());
+
+            if (numar > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if (numar < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else 
+            {
+                Console.WriteLine("0");
+            }
+
+        }
+    }
+}
